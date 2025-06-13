36eba0300623e9d790494fe03c9defaeaf8077ed
Nabielz1 <gomblohsyukar@gmail.com>
2025-06-13 12:57:13 +0700

    feat: skills marquee animation

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,67 +1,90 @@
 import "../App.css"; // Import Tailwind CSS styles
+import Marquee from "react-fast-marquee";
 
 function Skills() {
-  const hardSkills = [
-    { skill: "Machine Learning", progress: 90 },
-    { skill: "Python", progress: 95 },
-    { skill: "SQL", progress: 90 },
-    { skill: "Object Oriented Programming", progress: 95 },
-    { skill: "Microsoft Office", progress: 100 },
+  const skillsData = [
+    'Adobe After Effect',
+    'Canva',
+    'CSS',
+    'Figma',
+    'Javascript',
+    'Microsoft Office',
+    'MongoDB',
+    'MySQL',
+    'PHP',
+    'PostgreSQL',
+    'Python',
+    'React',
   ];
 
-  const softSkills = [
-    { skill: "Communication", progress: 100 },
-    { skill: "Teamwork", progress: 100 },
-    { skill: "Leadership", progress: 100 },
-    { skill: "Adaptive", progress: 100 },
-    { skill: "Problem Solver", progress: 100 },
-  ];
+  const skillsImage = (skill) => {
+    const skillassets = {
+      'Adobe After Effect': { src: '/assets/after-effects.svg' },
+      'Canva': { src: '/assets/canva.svg' },
+      'CSS': { src: '/assets/css.svg' },
+      'Figma': { src: '/assets/figma.svg' },
+      'Javascript': { src: '/assets/javascript.svg' },
+      'Microsoft Office': { src: '/assets/microsoftoffice.svg' },
+      'MongoDB': { src: '/assets/mongoDB.svg' },
+      'MySQL': { src: '/assets/mysql.svg' },
+      'PHP': { src: '/assets/php.svg' },
+      'PostgreSQL': { src: '/assets/postgresql.svg' },
+      'Python': { src: '/assets/python.svg' },
+      'React': { src: '/assets/react.svg' },
+    };
+
+    return skillassets[skill];
+  };
 
   return (
-    <section id="skills" className="py-20 px-48 bg-white">
-      <h2 className="text-3xl font-semibold mb-6 border-b-4 max-w-fit">Skills</h2>
-      <div className="grid gap-20 sm:grid-cols-2 lg:grid-cols-2">
-        <div>
-          <h3 className="text-xl font-semibold mb-4">Hard Skills</h3>
-          <ul>
-            {hardSkills.map((item, index) => (
-              <li key={index} className="mb-4">
-                <div className="flex justify-between">
-                  <span>{item.skill}</span>
-                  <span>{item.progress}%</span>
-                </div>
-                <div className="w-full bg-gray-300 rounded-full h-2">
-                  <div
-                    className="bg-gray-800 h-2 rounded-full"
-                    style={{ width: `${item.progress}%` }}
-                  ></div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        <div>
-          <h3 className="text-xl font-semibold mb-4">Soft Skills</h3>
-          <ul>
-            {softSkills.map((item, index) => (
-              <li key={index} className="mb-4">
-                <div className="flex justify-between">
-                  <span>{item.skill}</span>
-                  <span>{item.progress}%</span>
-                </div>
-                <div className="w-full bg-gray-300 rounded-full h-2">
-                  <div
-                    className="bg-gray-800 h-2 rounded-full"
-                    style={{ width: `${item.progress}%` }}
-                  ></div>
-                </div>
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div id="skills" className="py-20 px-48">
+      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl opacity-20">
       </div>
-    </section>
+          <h2 className="text-3xl font-semibold mb-6 border-b-4 pb-2 max-w-fit">Skills</h2>
+          
+      <div className="w-full">
+        <Marquee
+          gradient={false}
+          speed={80}
+          pauseOnHover={true}
+          pauseOnClick={true}
+          delay={0}
+          play={true}
+          direction="left"
+        >
+          {skillsData.map((skill, id) => {
+            const skillImg = skillsImage(skill);
+            return (
+              skillImg && (
+                <div className="w-36 min-w-fit h-fit flex flex-col items-center justify-center transition-all duration-500 m-3 sm:m-5 rounded-lg group relative hover:scale-[1.15] cursor-pointer" key={id}>
+                  <div className="h-full w-full rounded-lg border border-[#1f223c] bg-[#11152c] shadow-none shadow-gray-50 group-hover:border-violet-500 transition-all duration-500">
+                    <div className="flex -translate-y-[1px] justify-center">
+                      <div className="w-3/4">
+                        <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-violet-500 to-transparent" />
+                      </div>
+                    </div>
+                    <div className="flex flex-col items-center justify-center gap-3 p-6">
+                      <div className="h-8 sm:h-10">
+                        <img
+                          src={skillImg.src}
+                          alt={skill}
+                          width={40}
+                          height={40}
+                          className="h-full w-auto rounded-lg"
+                        />
+                      </div>
+                      <p className="text-white text-sm sm:text-lg">
+                        {skill}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )
+            );
+          })}
+        </Marquee>
+      </div>
+    </div>
   );
 }
 
